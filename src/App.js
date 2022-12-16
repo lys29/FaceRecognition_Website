a6b8dfb4703927fb78a2867b62852219f88720ed
lys29 <danaellysacamero@gmail.com>
2022-12-17 01:24:30 +0800

    no react router dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Nav from "./components/Nav.jsx";
 import Title from "./components/Title.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Nav" element={<Nav />} />
-        <Route path="/Title" element={<Title />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Nav />
+      <Title />
+    </div>
   );
 }
 
