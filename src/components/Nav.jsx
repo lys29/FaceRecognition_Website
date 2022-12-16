46647bab2d6672cc1bdc977a1ac2aa8a9a84b282
lys29 <danaellysacamero@gmail.com>
2022-12-17 00:06:38 +0800

    nav change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,16 @@
 import styles from './Nav.module.css';
 import Button from './Button.jsx';
-import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
         <div className={styles.container}>
             <div>
                 <ul>
-                    <Link to="/Home"><div className={styles.main}><Button variant='primary'><h6>Face <span>recognition</span> <br></br> <span1>Attendance</span1> <span>Checker</span></h6></Button></div></Link>
+                    <a href="/Home"><div className={styles.main}><Button variant='primary'><h6>Face <span>recognition</span> <br></br> <span1>Attendance</span1> <span>Checker</span></h6></Button></div></a>
                     <div className={styles.hover}>
-                        <Link to="/Us"><Button variant='secondary'>Contact Us</Button></Link>
-                        <Link to="/About"><Button variant='secondary'>About Us</Button></Link>
-                        <div className={styles.sign}><Link to="/Log"><Button variant='secondary'>Login</Button></Link></div>
+                        <a href="/Us"><Button variant='secondary'>Contact Us</Button></a>
+                        <a href="/About"><Button variant='secondary'>About Us</Button></a>
+                        <div className={styles.sign}><a href="/Log"><Button variant='secondary'>Login</Button></a></div>
                     </div>
                 </ul>
             </div>
